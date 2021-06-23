3a56889f2f3fdabc9f862136936f9474b84a7c1d
Nico Hämäläinen <selitys@pm.me>
2021-06-23 21:14:59 +0300

    arrays

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,39 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	// range lets you iterate over an array.
+	// On each iteration, range returns two values - the index and the value.
+	// We are choosing to ignore the index value by using _ blank identifier.
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
+}
+
+// Go can let you write variadic functions that can take a variable number of arguments.
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		// Safely handle empty slices
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			// Slices can be sliced! The syntax is slice[low:high].
+			// If you omit the value on one of the sides of the : it captures everything to that side of it
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
 }
