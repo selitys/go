61f238e723cb9461be6fee382e9424e71a1cdbb2
Nico Hämäläinen <selitys@pm.me>
2021-06-23 11:44:03 +0300

    Hello final

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,32 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const french = "French"
+const spanish = "Spanish"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "English"))
 }
